cb0298cd4bbde09a15236644ecd29e105e5ff0c1
eli bracha <elibracha45@gmail.com>
2020-08-26 00:39:11 +0300

    init

---
+++ src/test/java/com/github/elibracha/brewrey/BrewreyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elibracha.brewrey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewreyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
